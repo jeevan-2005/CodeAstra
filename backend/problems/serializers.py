018cbed9f2b56c2fd74b9b1098882cbb9560d734
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-06-24 20:56:56 +0530

    modified problem serializer

--- backend/problems/serializers.py
+++ backend/problems/serializers.py
@@ -7,8 +7,15 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = '__all__'
 
+class TestExampleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestExample
+        fields = '__all__'
+
 class ProblemDetialSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
+    test_examples = TestExampleSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Problem
         fields = '__all__'
@@ -18,5 +25,5 @@ class ProblemSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Problem
-        fields = ['problem_name', 'difficulty', 'tags']
+        fields = [ 'id' ,'problem_name', 'difficulty', 'tags']
 
