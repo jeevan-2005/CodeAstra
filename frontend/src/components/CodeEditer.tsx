d7d629f7b07358d27e2053e930f458f386c0276b
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-06-27 21:49:58 +0530

    Online compiler and online judge part done.

--- frontend/src/components/CodeEditer.tsx
+++ frontend/src/components/CodeEditer.tsx
@@ -1,30 +1,173 @@
-import React from 'react'
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './ui/select'
-import { Label } from './ui/label'
+"use client";
 
-const CodeEditer = () => {
+import React, { useEffect } from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { Label } from "./ui/label";
+import Editor from "react-simple-code-editor";
+
+import Prism from "prismjs";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-python";
+import "prismjs/themes/prism-okaidia.css";
+import { Button } from "./ui/button";
+import { MdSave } from "react-icons/md";
+
+import {
+  useGetSavedCodeQuery,
+  useSaveCodeMutation,
+} from "@/redux/submission/submissionApi";
+import LoadingSpinner from "./LoadingSpinner";
+
+type Props = {
+  problem_id: number;
+  code: string;
+  setCode: React.Dispatch<React.SetStateAction<string>>;
+  language: string;
+  setLanguage: React.Dispatch<React.SetStateAction<string>>;
+  user_id: number | undefined;
+};
+
+const CodeEditer = ({
+  problem_id,
+  code,
+  setCode,
+  language,
+  setLanguage,
+  user_id
+}: Props) => {
+
+  const [localError, setLocalError] = React.useState<string | null>(null);
+  const queryParams = React.useMemo(
+    () => ({
+      problem_id,
+      user_id: user_id,
+      language,
+    }),
+    [problem_id, user_id, language]
+  );
+
+  const {
+    data,
+    isLoading,
+    isError: getSaveCodeError,
+  } = useGetSavedCodeQuery(queryParams, {
+    refetchOnMountOrArgChange: true,
+  });
+
+  const [saveCode, { isLoading: isSavingCode, isError }] =
+    useSaveCodeMutation();
+
+  useEffect(() => {
+    if (data) {
+      if (!data.code) {
+        setCode("");
+      } else {
+        setCode(JSON.parse(data.code));
+      }
+    }
+    if (getSaveCodeError) {
+      setCode("");
+    }
+  }, [data, getSaveCodeError, setCode]);
+
+  const getPrismLanguage = (langKey: string) => {
+    switch (langKey) {
+      case "c":
+        return { grammar: Prism.languages.c, name: "c" };
+      case "cpp":
+        return { grammar: Prism.languages.cpp, name: "cpp" };
+      case "python":
+        return { grammar: Prism.languages.python, name: "python" };
+      case "java":
+        return { grammar: Prism.languages.java, name: "java" };
+      default:
+        return { grammar: Prism.languages.clike || {}, name: "clike" };
+    }
+  };
+
+  const handleSaveCode = async () => {
+    const stringified_code = JSON.stringify(code);
+    const data = {
+      code: stringified_code,
+      language: language,
+      problem_id: problem_id,
+      user_id: user_id,
+    };
+
+    await saveCode(data);
+  };
+
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center h-full w-full">
+        <LoadingSpinner size={20} />
+      </div>
+    );
+  }
+
+  if (isError) {
+    setLocalError("Something went wrong");
+  }
+
   return (
-    <div className='flex flex-col gap-4'>
-      <div className='flex justify-center items-center'>
-        {/* select language here - c++, python, c, java */}
-        <Label className='mr-4 text-md'>Select Language - </Label>
-        <Select defaultValue="c++">
-          <SelectTrigger className='w-[200px] cursor-pointer'>
-            <SelectValue placeholder="Select a language" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="c++">C++</SelectItem>
-            <SelectItem value="python">Python</SelectItem>
-            <SelectItem value="c">C</SelectItem>
-            <SelectItem value="java">Java</SelectItem>
-          </SelectContent>
-        </Select>
+    <div className="flex flex-col gap-4">
+      <div className="flex justify-between items-center">
+        <div>
+          <Button
+            disabled={isSavingCode}
+            className="cursor-pointer bg-green-600 hover:bg-green-700"
+            onClick={handleSaveCode}
+          >
+            {isSavingCode ? "Saving..." : "Save Code"} <MdSave />
+          </Button>
+          {localError && <p className="text-red-500">{localError}</p>}
+        </div>
+        <div className="flex items-center">
+          <Label className="mr-4 text-md">Select Language - </Label>
+
+          <Select value={language} onValueChange={setLanguage}>
+            <SelectTrigger className="w-[200px] cursor-pointer">
+              <SelectValue placeholder="Select a language" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectItem value="cpp">C++</SelectItem>
+              <SelectItem value="py">Python</SelectItem>
+              <SelectItem value="c">C</SelectItem>
+              <SelectItem value="java">Java</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
       <div>
-        <textarea className='w-full h-[350px] border-3 rounded-sm  resize-none p-2' placeholder='Write your code here'  />
+        <Editor
+          value={code}
+          onValueChange={(code) => setCode(code)}
+          highlight={(code) => {
+            const { grammar, name } = getPrismLanguage(language);
+            return Prism.highlight(code, grammar, name);
+          }}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 14,
+            backgroundColor: "#272822",
+            height: "350px", // Your fixed height
+            overflow: "auto",
+            color: "white",
+          }}
+          padding={15}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CodeEditer
+export default CodeEditer;
--- frontend/src/components/CustomTestAndSubmission.tsx
+++ frontend/src/components/CustomTestAndSubmission.tsx
@@ -1,42 +1,239 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import { Button } from "./ui/button";
-import { FaPlay } from "react-icons/fa";
+import { IoPlay } from "react-icons/io5";
 import { FaCircleCheck } from "react-icons/fa6";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import {
+  useRunCustomTestCaseMutation,
+  useSubmitCodeMutation,
+} from "@/redux/submission/submissionApi";
+import LoadingSpinner from "./LoadingSpinner";
 
-const CustomTestAndSubmission = () => {
+type Props = {
+  problem_id: number;
+  code: string;
+  language: string;
+  user_id: number | undefined;
+};
+
+type CustomTestError = {
+  status: number;
+  data: object;
+};
+
+type SubmitCodeError = {
+  status: number;
+  data: SubmitCodeData;
+};
+
+type SubmitCodeData = {
+  verdict: string;
+  details: string;
+};
+
+type CustomTestErrorData = {
+  status: string;
+  details: string;
+};
+
+type CustomTestOutput = {
+  status: string;
+  output: string;
+};
+
+const CustomTestAndSubmission = ({
+  problem_id,
+  code,
+  language,
+  user_id,
+}: Props) => {
+  const [customInput, setCustomInput] = React.useState<string>("");
+  const [customOutput, setCustomOutput] = React.useState<object | undefined>(
+    undefined
+  );
+  const [verdict, setVerdict] = React.useState<SubmitCodeData | undefined>(
+    undefined
+  );
+  const [tab, setTab] = React.useState<string>("input");
+
+  const [runCustomTestCase, { isLoading, isError, error, data }] =
+    useRunCustomTestCaseMutation();
+
+  const [
+    submitCode,
+    {
+      isLoading: isSubmitCodeLoading,
+      isError: isSubmitCodeError,
+      error: submitCodeError,
+      data: submitResponseData,
+    },
+  ] = useSubmitCodeMutation();
+
+  const handleRunCode = async () => {
+    const runCustomTestCaseRequest = {
+      code,
+      user_input: customInput,
+      language,
+    };
+    console.log(runCustomTestCaseRequest);
+    await runCustomTestCase(runCustomTestCaseRequest);
+    setTab("output");
+  };
+
+  const handleCodeSubmit = async () => {
+    const submitRequestData = {
+      problem_id: problem_id,
+      code: code,
+      language: language,
+      user_id: user_id,
+    };
+    await submitCode(submitRequestData);
+    setTab("verdict");
+  };
+
+  useEffect(() => {
+    if (data) {
+      setCustomOutput(data as CustomTestOutput);
+    } else if (isError) {
+      setCustomOutput((error as CustomTestError).data as CustomTestErrorData);
+    }
+  }, [data, isError, error]);
+
+  useEffect(() => {
+    if (submitResponseData) {
+      setVerdict(submitResponseData);
+    } else if (isSubmitCodeError) {
+      setVerdict((submitCodeError as SubmitCodeError).data);
+    }
+  }, [submitResponseData, isSubmitCodeError, submitCodeError]);
+
   return (
     <div className="flex flex-col gap-2 w-full">
       <div className="w-full">
-        <Tabs defaultValue="input" className="w-full">
+        <Tabs value={tab} className="w-full">
           <TabsList className="w-full">
-            <TabsTrigger value="input">Input</TabsTrigger>
-            <TabsTrigger value="output">Output</TabsTrigger>
-            <TabsTrigger value="verdict">Verdict</TabsTrigger>
+            <TabsTrigger
+              value="input"
+              className="cursor-pointer mr-1 hover:bg-[#eeebeb]"
+              onClick={() => setTab("input")}
+            >
+              Input
+            </TabsTrigger>
+            <TabsTrigger
+              value="output"
+              className="cursor-pointer hover:bg-[#eeebeb]"
+              onClick={() => setTab("output")}
+            >
+              Output
+            </TabsTrigger>
+            <TabsTrigger
+              value="verdict"
+              className="cursor-pointer ml-1 hover:bg-[#eeebeb]"
+              onClick={() => setTab("verdict")}
+            >
+              Verdict
+            </TabsTrigger>
           </TabsList>
-          <TabsContent value="input">
-            <textarea
-              className="w-full h-[100px] border-3 rounded-sm p-2"
-              placeholder="custom input"
-            />
-          </TabsContent>
-          <TabsContent value="output">
-            <div className="w-full min-h-[100px] max-h-[200px] border-3 rounded-sm p-2"></div>
-          </TabsContent>
-          <TabsContent value="verdict">
-            <div className="w-full min-h-[100px] max-h-[200px] border-3 rounded-sm p-2"></div>
-          </TabsContent>
+          {isLoading || isSubmitCodeLoading ? (
+            <div className="w-full h-[100px] border-3 rounded-sm flex items-center justify-center">
+              <LoadingSpinner size={20} />
+            </div>
+          ) : (
+            <>
+              <TabsContent value="input">
+                <textarea
+                  className="w-full h-[100px] border-3 rounded-sm p-2"
+                  placeholder="custom input"
+                  value={customInput}
+                  onChange={(e) => setCustomInput(e.target.value)}
+                />
+              </TabsContent>
+              <TabsContent value="output">
+                <div className="w-full min-h-[100px] text-wrap border-3 rounded-sm p-2 [white-space:pre-wrap]">
+                  {customOutput ? (
+                    (customOutput as CustomTestErrorData).status !=
+                    "success" ? (
+                      <>
+                        <p className="text-lg font-medium">
+                          {(customOutput as CustomTestErrorData).status}
+                        </p>
+                        <pre className="text-red-700 whitespace-pre-wrap break-words text-sm">
+                          {(customOutput as CustomTestErrorData).details}
+                        </pre>
+                      </>
+                    ) : (
+                      (customOutput as CustomTestOutput).output && (
+                        <>
+                          <pre>{(customOutput as CustomTestOutput).output}</pre>
+                        </>
+                      )
+                    )
+                  ) : (
+                    <p className="text-gray-500 text-sm">
+                      Run the code to see output or errors.
+                    </p>
+                  )}
+                </div>
+              </TabsContent>
+              <TabsContent value="verdict">
+                <div className="w-full min-h-[100px] text-wrap border-3 rounded-sm p-2 [white-space:pre-wrap]">
+                  {verdict ? (
+                    verdict.verdict == "Accepted" ? (
+                      <>
+                        <p className="text-lg font-medium text-green-600">
+                          {verdict.verdict}
+                        </p>
+                        <pre className="whitespace-pre-wrap break-words text-sm">
+                          {verdict.details}
+                        </pre>
+                      </>
+                    ) : verdict.verdict == "Wrong Answer" ? (
+                      <>
+                        <p className="text-lg font-medium text-red-600">
+                          {verdict.verdict}
+                        </p>
+                        <pre className="text-red-700 whitespace-pre-wrap break-words text-sm">
+                          {verdict.details}
+                        </pre>
+                      </>
+                    ) : (
+                      <>
+                        <p className="text-lg font-medium text-yellow-600">
+                          {verdict.verdict}
+                        </p>
+                        <pre className="whitespace-pre-wrap break-words text-sm">
+                          {verdict.details}
+                        </pre>
+                      </>
+                    )
+                  ) : (
+                    <p className="text-gray-500 text-sm">
+                      Submit the code to see verdict.
+                    </p>
+                  )}
+                </div>
+              </TabsContent>
+            </>
+          )}
         </Tabs>
       </div>
       <div className="flex gap-2 items-center justify-between">
-        <Button className="w-[200px] text-[15px] cursor-pointer">
-          Run <FaPlay />{" "}
+        <Button
+          className="w-[200px] text-[15px] cursor-pointer"
+          onClick={handleRunCode}
+          disabled={isLoading}
+        >
+          {isLoading ? "Running..." : "Run"} <IoPlay />{" "}
         </Button>
         <Button
           className="w-[200px] text-[15px] cursor-pointer"
           variant="destructive"
+          onClick={handleCodeSubmit}
+          disabled={isSubmitCodeLoading}
         >
-          Submit <FaCircleCheck />
+          {isSubmitCodeLoading ? "Submitting..." : "Submit"} <FaCircleCheck />
         </Button>
       </div>
     </div>
