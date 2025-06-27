94052c93111314dad5190b276b411003ade2ba13
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-06-27 21:47:57 +0530

    reducer path of submission added

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -2,15 +2,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authSlice";
 import { authApi } from "./auth/authApi";
 import problemApi from "./problems/problemApi";
+import submissionApi from "./submission/submissionApi";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
     [problemApi.reducerPath]: problemApi.reducer,
+    [submissionApi.reducerPath]: submissionApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApi.middleware, problemApi.middleware),
+    getDefaultMiddleware().concat(authApi.middleware, problemApi.middleware, submissionApi.middleware),
 });
 
 // Types for use in components
