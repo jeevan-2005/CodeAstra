2b0129b536f1dd929a26029443f25bfd2a3696dc
jeevan-2005 <jeevanchoudhary2005@gmail.com>
2025-07-02 12:53:35 +0530

    all submission page

---
+++ frontend/src/app/submissions/page.tsx
@@ -0,0 +1,10 @@
+import SubmissionsPage from '@/components/SubmissionsPage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div><SubmissionsPage /></div>
+  )
+}
+
+export default page
